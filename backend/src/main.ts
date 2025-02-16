490e179a7b12336cc83b428f871914f71f93db39
illdub <illia.dubrovin@dreamhost.com>
2025-02-16 13:31:38 +0100

    fix cors error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://esmana-main.org/'],
+    origin: ['http://localhost:3000', 'https://esmana-main.org'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
