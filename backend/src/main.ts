7ce6873028183fb4f7496851c130a6238b9d7c91
illdub <illia.dubrovin@dreamhost.com>
2025-02-21 13:50:24 +0100

    edit cookies

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -17,9 +17,6 @@ async function bootstrap() {
     saveUninitialized: false,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24, // 24 hours
-      httpOnly: true,
-      secure: !isDevelop,
-      sameSite: isDevelop ? 'strict' : 'none',
     }
   }));
 
