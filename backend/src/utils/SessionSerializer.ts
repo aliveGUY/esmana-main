b27d6d0bf9031565acf9484f81006e2532f0e4e5
illdub <illia.dubrovin@dreamhost.com>
2025-02-21 14:55:30 +0100

    edit cookies

--- backend/src/utils/SessionSerializer.ts
+++ backend/src/utils/SessionSerializer.ts
@@ -2,7 +2,6 @@ import { PassportSerializer } from "@nestjs/passport";
 import { Injectable } from "@nestjs/common";
 import { AuthService } from "src/services/authService";
 import { User } from "src/models/User";
-import { isEmpty } from "lodash";
 
 @Injectable()
 export class SessionSerializer extends PassportSerializer {
@@ -10,15 +9,12 @@ export class SessionSerializer extends PassportSerializer {
     super();
   }
 
-  serializeUser(user: User, done: (err: any, user: User | null) => void) {
-    done(null, user);
+  serializeUser(user: User, done: (err: any, id?: any) => void) {
+    done(null, user.id);
   }
 
-
-  async deserializeUser(user: User, done: (err: any, user: User | null) => void) {
-    const foundUser = await this.authService.findUserById(user.id);
-
-    if (isEmpty(foundUser)) return done(null, null);
-    return done(null, foundUser)
+  async deserializeUser(id: number, done: (err: any, user?: User | null) => void) {
+    const foundUser = await this.authService.findUserById(id);
+    return foundUser ? done(null, foundUser) : done(null, null);
   }
 }
