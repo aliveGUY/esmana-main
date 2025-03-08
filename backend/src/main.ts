544780ee48ea1a940c479d40faa3371cb47c0f12
illdub <illia.dubrovin@dreamhost.com>
2025-03-08 15:41:44 +0100

    edit origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import * as passport from 'passport'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const isDevelop = process.env.NODE_ENV === 'development'
-  const allowedOrigins = isDevelop ? 'http://localhost:3000' : 'https://esmana-main.org'
+  const clientOrigin = 'https://esmana-main.org'
 
   if (!process.env.SESSION_SECRET) throw new Error()
   const expressApp = app.getHttpAdapter().getInstance();
@@ -25,7 +25,7 @@ async function bootstrap() {
   }));
 
   app.enableCors({
-    origin: allowedOrigins,
+    origin: [clientOrigin],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
