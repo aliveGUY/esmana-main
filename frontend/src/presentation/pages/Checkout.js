b1f95d5fb176636b3c62e305f4a008ec3ae350f7
illdub <illia.dubrovin@dreamhost.com>
2025-03-08 16:05:36 +0100

    edit origins

--- frontend/src/presentation/pages/Checkout.js
+++ frontend/src/presentation/pages/Checkout.js
@@ -8,10 +8,7 @@ import { useCreateCheckoutSessionMutation } from "../../state/asynchronous/users
 import { isEmpty } from "lodash";
 import Paper from "@mui/material/Paper";
 
-// public key
-const stripePromise = loadStripe(
-  "pk_test_51QxmlVJ0C91nYQSaZ2EIU2SNoyb0hyt5I6GE4BqD5OQvflvvXDqder9yqV6fMoVp9CWXIOMis1pfjmcyveRyyYjV00dy5XBmNI"
-);
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
 const Checkout = () => {
   const [createCheckoutSession, { data }] = useCreateCheckoutSessionMutation();
